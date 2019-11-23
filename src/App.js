d73680b095a72a451acd6c81e265822f41020320
David <david@localhost.localdomain>
2019-11-22 19:11:10 -0600

    Agregue la API de openwathermap y la integre

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
-import React,{ Component } from 'react';
-import Header from './components/Header.jsx';
-import Form from './components/Form.jsx';
-import Error from './components/Error.jsx'
+import React, { Component } from "react";
+import Header from "./components/Header.jsx";
+import Form from "./components/Form.jsx";
+import Error from "./components/Error.jsx";
 
 class App extends Component {
-  state = { 
-    Error:false
-   }
-  DataCons=respuesta=>{
-    if(respuesta.Ciudad==='' || respuesta.Pais ===''){
-      this.setState({
-        Error:true
-      })
-    }else{
-      console.log(respuesta)
-    }
-    
+  state = {
+    Error: false,
+    respuesta:{}
+  };
+
+  componentDidUpdate() {
+    this.APICon();
   }
-  render() { 
-    const error = this.state.Error
-    if(error === true){
-      <Error/>
+
+  APICon =()=>{
+    const {Ciudad,Pais} =this.state.respuesta;
+    if(!Ciudad|| !Pais)return null;
+    const appID='71636f1e57033ef063ae3271d79b74e'
+    let Url =`https://samples.openweathermap.org/data/2.5/weather?q=${Ciudad},${Pais}&appid=${appID}`;
+    console.log(Url)
+    //Query con Fecth API
+
+    //Read URL API
+
+    //Consult API
+  }
+
+  DataCons = respuesta => {
+    //verificando los campos
+    if (respuesta.Ciudad === "" || respuesta.Pais === "") {
+      this.setState({
+        Error: true
+      });
+    } else {
+      //Add the Object to theState
+      this.setState({
+        respuesta
+      })
     }
-    return ( <div className="app">
-    <Header
-    titulo={"Clima"}
-    />
-    <Form
-    DataCons={this.DataCons}
-    />
-  </div> );
+  };
+  render() {
+    const error = this.state.Error;
+    let resultado;
+    if (error === true) {
+      resultado = <Error />;
+    }
+    return (
+      <div className="app">
+        <Header titulo={"Clima"} />
+        <Form DataCons={this.DataCons} />
+        {resultado}
+      </div>
+    );
   }
 }
 
