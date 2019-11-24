dbc4af53984f584575ccf91ec292c30a9ff95be7
David <david@localhost.localdomain>
2019-11-24 04:12:50 -0600

    Updates

---
+++ precache-manifest.30313ba5e686eb99ab75f116e13fdd7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac3e36130d464673c252251b7798de2",
+    "url": "/app-clima/index.html"
+  },
+  {
+    "revision": "8e32bd369ca5d1109560",
+    "url": "/app-clima/static/css/main.0010486f.chunk.css"
+  },
+  {
+    "revision": "6a92e496fa9603b83448",
+    "url": "/app-clima/static/js/2.592fb0ea.chunk.js"
+  },
+  {
+    "revision": "8e32bd369ca5d1109560",
+    "url": "/app-clima/static/js/main.623afd6a.chunk.js"
+  },
+  {
+    "revision": "79270c9c2fdd5b09203f",
+    "url": "/app-clima/static/js/runtime-main.f3a36642.js"
+  }
+]);
